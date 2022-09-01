41810b4bebce8653267753047e5e3e008d01d411
ksnowflake <112566280+ksnowflake@users.noreply.github.com>
2022-09-01 16:47:05 -0400

    Update streamlit_app.py
    Removed raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())# Just writes the data to the screen.
 
 # Takes the json data and normalize it. 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
